8bc79ce45561efeffd3ff94974a8790130e4bede
Jonathan Livingstone <j_livingstone@outlook.com>
2019-05-13 09:08:16 -0400

    completed module 2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,7 @@
 <template>
 <div>
     <h1>{{title}}</h1>
+    <li v-for="book in books">{{book.title}}:{{book.author}}</li>
 </div>
 </template>
 
@@ -9,7 +10,12 @@ export default {
     name: 'BookList',
     data() {
         return {
-            title: 'All Books'
+            title: 'All Books',
+            books: [
+            {title:'Self-Reliance', author: 'Ralph Waldo Emerson'},
+            {title:'American Gods', author: 'Neil Gaiman'},
+            {title:'Amusing ourselves to Death', atuhor: 'Neil Postman'},
+        ]
         }
     }
 }
@@ -18,5 +24,13 @@ export default {
 <style>
 h1, h2{
     font-weight: normal;
+}
+ul{
+    list-style-type: none;
+    padding: 0;
+}
+li{
+    display: block;
+    margin: 0 10px;
 }
 </style>
